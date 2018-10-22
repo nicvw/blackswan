6b3a9302abf5bdf584dd281482698a5a1900604e
Nic von Waltsleben <waltsleben@gmail.com>
2018-10-22 08:59:47 +0200

    web server

--- main.tf
+++ main.tf
@@ -3,6 +3,10 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+provider "dns" {
+  version = "~> 2.0"
+}
+
 # Create a vpc with a cidr 10.0.0.0/16
 resource "aws_vpc" "nicvw" {
   cidr_block           = "10.0.0.0/16"
@@ -15,30 +19,58 @@ resource "aws_internet_gateway" "gw" {
 }
 
 # create 2 subnets within the VPC in 2 different AZ's
-resource "aws_subnet" "primary_subnet" {
+resource "aws_subnet" "public-1a" {
   vpc_id            = "${aws_vpc.nicvw.id}"
   cidr_block        = "10.0.0.0/24"
   availability_zone = "us-east-1a"
 }
 
-resource "aws_subnet" "secondary_subnet" {
+resource "aws_subnet" "public-1b" {
   vpc_id            = "${aws_vpc.nicvw.id}"
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1b"
 }
 
 # Elastic load balancer with port 80 and 443 exposed and a public ip address
-resource "aws_lb" "frontend" {
-  name_prefix        = "lb-",
+resource "aws_alb" "frontend" {
+  name        = "frontend-alb-www",
   internal           = false
-  load_balancer_type = "application"
-  subnets	           = ["${aws_subnet.primary_subnet.id}", "${aws_subnet.secondary_subnet.id}"]
+  subnets	           = ["${aws_subnet.public-1a.id}", "${aws_subnet.public-1b.id}"]
   depends_on         = ["aws_internet_gateway.gw"]
 }
 
-resource "aws_lb_target_group" "frontend" {
-  name_prefix = "lb-tg-"
-  port        = 8000
+resource "aws_security_group" "inbound" {
+  name = "sec_group_inbound"
+  description = "Inbound Internet traffic"
+  vpc_id = "${aws_vpc.nicvw.id}"
+
+  ingress {
+    from_port = 0
+    to_port = 443
+    protocol = "tcp"
+  }
+
+  ingress {
+    from_port = 0
+    to_port = 80
+    protocol = "tcp"
+  }
+
+  ingress {
+    from_port = 0
+    to_port = 22
+    protocol = "tcp"
+  }
+}
+
+resource "aws_security_group" "web-servers" {
+  name   = "sec_group-www"
+  vpc_id = "${aws_vpc.nicvw.id}"
+}
+
+resource "aws_alb_target_group" "frontend_web" {
+  name = "lb-tg-frontend"
+  port        = 80
   protocol    = "HTTP"
   vpc_id      = "${aws_vpc.nicvw.id}"
 
@@ -46,8 +78,8 @@ resource "aws_lb_target_group" "frontend" {
   }
 }
 
-resource "aws_lb_listener" "frontend_https" {
-  load_balancer_arn = "${aws_lb.frontend.arn}"
+resource "aws_alb_listener" "frontend_web" {
+  load_balancer_arn = "${aws_alb.frontend.arn}"
   port              = 443
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
@@ -55,25 +87,24 @@ resource "aws_lb_listener" "frontend_https" {
 
   default_action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.frontend.arn}"
+    target_group_arn = "${aws_alb_target_group.frontend_web.arn}"
   }
 }
 
-resource "aws_lb_listener" "frontend_http" {
-  load_balancer_arn = "${aws_lb.frontend.arn}"
+resource "aws_alb_listener" "frontend_http" {
+  load_balancer_arn = "${aws_alb.frontend.arn}"
   port              = 80
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.frontend.arn}"
+    target_group_arn = "${aws_alb_target_group.frontend_web.arn}"
   }
 }
 
 # create a domain in route 53 (any will do, private won't need registration) and get a cert for the domain to apply to the ELB
 resource "aws_route53_zone" "zone" {
   name   = "aws.sigterm.co.za"
-  # vpc_id = "${aws_vpc.nicvw.id}"
 }
 
 resource "aws_acm_certificate" "default" {
@@ -91,8 +122,8 @@ resource "aws_route53_record" "www" {
   type    = "A"
 
   alias {
-    name                   = "${aws_lb.frontend.dns_name}"
-    zone_id                = "${aws_lb.frontend.zone_id}"
+    name                   = "${aws_alb.frontend.dns_name}"
+    zone_id                = "${aws_alb.frontend.zone_id}"
     evaluate_target_health = true
   }
 }
@@ -111,6 +142,59 @@ resource "aws_acm_certificate_validation" "default" {
 }
 
 # an EC2 instance with nginx installed , using amazon linux (automatically)
+data "aws_ami" "amazon_linux" {
+  most_recent      = true
+
+  filter {
+    name = "name"
+    values = ["amzn2-ami-minimal-hvm-2.0.*-x86_64-ebs"]
+  }
+}
+
+data "dns_a_record_set" "www" {
+  host = "${aws_alb.frontend.dns_name}"
+}
+
+output "LB IP" {
+  value = "${data.dns_a_record_set.www.addrs}"
+}
+
+resource "aws_instance" "nginx" {
+  ami           = "${data.aws_ami.amazon_linux.image_id}"
+  instance_type = "t2.micro"
+  subnet_id     = "${aws_subnet.public-1a.id}"
+
+
+  provisioner "remote-exec" {
+
+    inline = [
+      "sudo yum install nginx -y",
+      "sudo service nginx start",
+    ]
+
+    connection {
+      host = "${aws_eip.nginx-bootstrap-ip.public_ip}"
+    }
+  }
+}
+
+resource "aws_eip" "nginx-bootstrap-ip" {
+  vpc	     = true
+}
+
+resource "aws_eip_association" "nginx-bootstrap-ip" {
+  allocation_id = "${aws_eip.nginx-bootstrap-ip.id}"
+  instance_id = "${aws_instance.nginx.id}"
+
+}
+
+
+resource "aws_lb_target_group_attachment" "frontend_www" {
+  target_group_arn = "${aws_alb_target_group.frontend_web.arn}"
+  target_id        = "${aws_instance.nginx.id}"
+  port             = 80
+}
+
 # Mysql instance with configurable DB name , username and password accessible by the vpc only
 # output the ELB IP , mysql url, username and password at end of run
 # ec2 instance size and root block size aswell as mysql dbname , username,password , instance size and space configurable with a TF vars file .
